0c82b7e8fe217ef96636781f30ccf3df7caf4abf
chuanm <chuangmatthew@yahoo.com>
2023-08-23 17:18:03 -0400

    Clicking on the placed stairs now takes you to the next level. Each level gets larger and harder than the last up until 10x10 with 10% rounded up bombs

--- Main.gd
+++ Main.gd
@@ -5,23 +5,36 @@ var Board = preload("res://board.tscn")
 var depth = 0
 var population = 3
 var stone = 0
-var b
+
+var boards = []
 
 signal queue_building(building_name)
 
 func generate_board(difficulty: int):
-	pass
+	var b = Board.instantiate()
+	add_child(b)
+	await b._ready()
+	
+	if difficulty == 0:
+		b.init_board(6,6,4)
+	elif difficulty == 1:
+		b.init_board(7,7,5)
+	elif difficulty == 2:
+		b.init_board(8,8,7)
+	elif difficulty == 3:
+		b.init_board(9,9,9)
+	else:
+		b.init_board(10,10,10)
+		
+	var center = get_viewport_rect().size/2
+	var offset = Vector2(center.x-(b.rows * b.TILE_SIZE/2), center.y-(b.columns * b.TILE_SIZE/2))
+	b.position = offset
+	boards.push_back(b)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# place board in center with correct offset accounting for tile size and board size
-	b = Board.instantiate()
-	add_child(b)
-	await b._ready()
-	b.init_board(6,6,4)
-	var center = get_viewport_rect().size/2
-	var offset = Vector2(center.x-(b.rows * b.TILE_SIZE/2), center.y-(b.columns * b.TILE_SIZE/2))
-	b.position = offset
+	generate_board(0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -40,10 +53,11 @@ func build(building_name):
 func next_level():
 	print("MAIN SCENE RECEIVED NEXT LEVEL CALL")
 	depth += 1
+	generate_board(depth)
 
 func _on_end_level_btn_pressed():
-	if b.build_mode == false:
-		b.enter_build_mode()
+	if boards[len(boards)-1].build_mode == false:
+		boards[len(boards)-1].enter_build_mode()
 		print("MANUALLY ENTERING BUILD MODE")
 	else:
 		print("ALREADY IN BUILD MODE")
